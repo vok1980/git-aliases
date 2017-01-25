50e8271f4286977bea39fa756749541a3c3dd227
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2017-01-25 12:28:20 +0700

    add general aliases

--- apply.sh
+++ apply.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git config --global alias.st 'status'
+git config --global alias.cl 'clone'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.br 'branch'
+
 git config --global alias.la '!git config -l | grep alias | cut -c 7-'
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.visual '!gitk'
