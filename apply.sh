c0f658be01c9e1d1fe439b950733b9535d716b6d
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2017-03-20 13:34:51 +0700

    change lg

--- apply.sh
+++ apply.sh
@@ -23,7 +23,7 @@ git config --global alias.rh1 'reset HEAD^ --hard'
 git config --global alias.rh2 'reset HEAD^^ --hard'
 
 git config --global alias.lf 'log --follow'
-git config --global alias.lg 'log --graph'
+git config --global alias.lg 'log --graph --full-history --color --date=short --pretty=format:"%x1b[33m%h%x09%x09%x1b[34m%ad%x09%an%x1b[32m%d%x1b[0m%x1b[0m%x09%s"'
 
 git config --global merge.tool vimdiff
 git config --global diff.tool vimdiff
