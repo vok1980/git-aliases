9c2f7d00bd8d93986f5432e6a7cd421fb21af085
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2018-12-05 18:39:14 +0700

    rm repository ppa:2gis/cpp-dev-env

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 sudo apt update && sudo apt upgrade
 
-sudo add-apt-repository ppa:2gis/cpp-dev-env
-
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
