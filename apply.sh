0a22aa8878c98dce976ff83ddcd5edfd9bd08a1f
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2020-04-16 10:37:16 +0700

    add upc & pullupc aliases

--- apply.sh
+++ apply.sh
@@ -10,9 +10,11 @@ git config --global alias.la '!git config -l | grep alias | cut -c 7-'
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.visual '!gitk'
 git config --global alias.k '!gitk'
-git config --global alias.up '!git submodule sync --recursive && git submodule update --init --recursive --checkout'
-git config --global alias.pullup '!git pull && git up'
 
+git config --global alias.up '!git submodule update --init --recursive'
+git config --global alias.upc '!git submodule update --init --recursive --checkout'
+git config --global alias.pullup '!git pull && git up'
+git config --global alias.pullupc '!git pull && git upc'
 
 git config --global alias.bf '!git checkout -b $1 && git push -u origin '
 git config --global alias.urri 'submodule update --recursive --rebase --init'
