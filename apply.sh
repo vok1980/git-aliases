851dc4d078de72260b105d195559304eca4c0e86
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2020-09-22 14:10:33 +0700

    add crx alias

--- apply.sh
+++ apply.sh
@@ -29,7 +29,8 @@ git config --global alias.rh2 'reset HEAD^^ --hard'
 git config --global alias.lf 'log --follow'
 git config --global alias.lg 'log --graph --full-history --color --date=short --pretty=format:"%x1b[33m%h%x09%x09%x1b[34m%ad%x09%an%x1b[32m%d%x1b[0m%x1b[0m%x09%s"'
 
-git config --global alias.cr '!git clean -dffx && git rh &&  git submodule foreach --recursive "git clean -dffx && git rh"'
+git config --global alias.cr '!git clean -dff && git rh &&  git submodule foreach --recursive "git clean -dff && git rh"'
+git config --global alias.crx '!git clean -dffx && git rh &&  git submodule foreach --recursive "git clean -dffx && git rh"'
 
 git config --global merge.tool vimdiff
 git config --global diff.tool vimdiff
